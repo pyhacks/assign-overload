5d9502b8b438557417cb75ee02653fb21b22ae16
pyhacks <enginyildirim111@gmail.com>
2026-04-03 20:31:31 +0300

    Add files via upload

--- assign_overload/patch.py
+++ assign_overload/patch.py
@@ -31,8 +31,6 @@ def patch_file_ast(filename, trans=AssignTransformer):
 
 
 def patch_module_ast(module, trans=AssignTransformer):
-    if not hasattr(module, '__file__'):
-        return module
     filename = module.__file__.replace('.pyc', '.py')
     return patch_file_ast(filename, trans)
 
@@ -41,9 +39,9 @@ def patch_and_reload_module(module = None, trans=AssignTransformer):
     if module is None:
         module_name = sys._getframe(1).f_globals["__name__"]
         module = sys.modules[module_name]    
-    if not hasattr(module, "patched") or not module.patched:
-        module.patched = True
+    if not hasattr(module, "patched") or not module.patched:        
         patched_ast = patch_module_ast(module)
+        module.patched = True
         module.modified_source = ast.unparse(patched_ast)
         patched_code = compile(patched_ast, module.__name__, "exec")
         module.executing_patch = True
